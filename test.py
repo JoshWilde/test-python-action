e84d14bbbca4876e072089cd126edef9cda45b63
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 14:55:58 +0100

    moved import

--- test.py
+++ test.py
@@ -4,11 +4,10 @@ import sklearn
 import pdfminer
 import nltk
 #import spacy
-from pdfminer.high_level import extract_text
 import os
 os.system(f"echo '🎉 All imports OK'")
 
 paper_path = os.environ['PAPER_PATH']
 os.system(f"echo '📄 PDF file located here: {paper_path}'")
 
-POI_PDF = [extract_text(paper_path)] # Extracts text from the PDF file
+POI_PDF = [pdfminer.high_level.extract_text(paper_path)] # Extracts text from the PDF file
