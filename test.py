e36f65a2752f5bacc8d021b6adc9f99617ef5808
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 14:25:20 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import numpy as np
 import glob
+import os
+os.system(f"echo '🎉 All imports OK'")
+
+paper_path = os.environ['PAPER_PATH']
+os.system(f"echo '📄 PDF file located here: {paper_path}'")
+
 import spacy
 import nltk
 import pdfminer
