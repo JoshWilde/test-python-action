0f586bb935d883b07838a49936040c031a79e654
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 15:07:30 +0100

    added from pdfminer.high_level import extract_text

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import pdfminer
 import pdfplumber
 import PyPDF2
 import nltk
+from pdfminer.high_level import extract_text
 #import spacy
 import os
 os.system(f"echo '🎉 All imports OK'")
@@ -12,4 +13,4 @@ os.system(f"echo '🎉 All imports OK'")
 paper_path = os.environ['PAPER_PATH']
 os.system(f"echo '📄 PDF file located here: {paper_path}'")
 
-POI_PDF = [pdfminer.high_level.extract_text(paper_path)] # Extracts text from the PDF file
+POI_PDF = [extract_text(paper_path)] # Extracts text from the PDF file
