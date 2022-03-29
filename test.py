59fda2826c87af74c94c3d0b239c080eef7f7c6b
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 15:32:58 +0100

    added nltk.download('omw-1.4')

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 #import spacy
 import os
