b5121d9f65b040d62746eaec8920a95c16a78432
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 16:14:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ import numpy as np
 import glob
 
 import spacy
-import en_core_web_lg
+
 import os
 os.system(f"echo '🎉 All imports OK'")
 
 paper_path = os.environ['PAPER_PATH']
 os.system(f"echo '📄 PDF file located here: {paper_path}'")
 
-model = en_core_web_lg.load()
+os.system('python -m spacy download en_core_web_lg')
 
+#model = en_core_web_lg.load()
+model = spacy.load('en_core_web_lg')
 print('SUCCESS!')
 
 #!python -m spacy download en_core_web_lg
