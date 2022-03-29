1353381938d928ee55fc761715188313f4bc53e1
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 15:49:53 +0100

    added lines for vector calculations, (commented out)

--- Paper_Vectors.py
+++ Paper_Vectors.py
@@ -72,7 +72,7 @@ def Get_Lemma_Words(POI_PDF):
 
 
 
-def Get_Top_Words_tf(Paper_interest, df=1, num_top20=20):
+def Get_Top_Words_tf(Paper_interest, num_top20=20):
     ''' 
     Parameters
     ----------
@@ -110,3 +110,76 @@ top20_tf = Get_Top_Words_tf(paper_path)
 #        X = np.array(fdist.most_common()) # Sorts the words in order of frequency
 #        top20_tf = X[:num_top20,0] # Saves the top N words as a list
 print(top20_tf)    
+
+"""
+def Generate_Paper_Vector(Paper_interest, model, num_top20=20):
+    ''' 
+    Parameters
+    ----------
+        Paper_interest : string
+            File path to the location of the PDF 
+            
+        model :
+            A
+        
+        df : Dictionary
+            A
+            
+        get_word_fun : function
+            A
+        
+        num_top20 : int
+            A
+            
+    Returns
+    ----------
+        pap_vector : array_like
+            An array of shape (300) representing where the given paper lies in the
+            model vector space.
+            
+        doc_top20 : string
+            A string containing the 20 words that were 
+        
+    ''' 
+    #average_vector = np.zeros((300)) # Creates an array for 300 zeros
+    top20_tf = Get_Top_Words_tf(Paper_interest, num_top20) # Gets the top N Words
+    #print(top20_tf)
+    doc_top20= '' # Creates empty string
+    if top20_tf != -2: # If the paper has lemmanised words
+        for i in top20_tf: # For each word in the top N
+                doc_top20 = doc_top20 + i +' ' # Appends each top N word to list
+    pap_vector = model(doc_top20).vector # generates a vector for the paper
+    #average_vector = average_vector + pap_vector 
+    
+    return pap_vector, doc_top20
+
+# Generate TF Vectors Paper
+def Paper_vectors_TF(paper_list, model,num_top20=20):
+    ''' 
+    Parameters
+    ----------
+        paper_list : array_like
+            Array of file paths to PDF files
+            
+        gen_pap_vec : function
+            A function to generate the vectors for paper that we are trying to find a reviewer for
+            
+    Returns
+    ----------
+        Paper_Dict : Dictionary
+            All the keys should be the DOI numbers for each paper taken from the file name. The items are vectors
+            of shape (300) which is the vector for where this paper lies in the model vector space.
+            
+        Paper_20_Dict : Dictionary
+            All the keys should be the DOI numbers for each paper taken from the file name. The items are the 
+            top 20 words from the paper that have been used to generate the vector representation.
+    ''' 
+    Paper_Dict = {}  # Defines an empty dictionary
+    Paper_20_Dict = {}  # Defines an empty dictionary
+    for k in range(len(paper_list)): # For each paper
+        print(paper_list[k]+ ' - ' +str(k))
+        paper_vector, doc_top20 = Generate_Paper_Vector(paper_list[k], model, num_top20) # Generates paper vector and shows the top N words
+        Paper_Dict[paper_list[k][-9:-4]] = paper_vector # Adds this vector to the dictionary
+        Paper_20_Dict[paper_list[k][-9:-4]] = doc_top20 # Adds the top N words to the dictionary
+    return Paper_Dict, Paper_20_Dict
+"""
