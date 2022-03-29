dc93d3171fe501fe4784ad67f6741e598b35a2df
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-29 16:05:39 +0100

    addded model download

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ os.system(f"echo '🎉 All imports OK'")
 paper_path = os.environ['PAPER_PATH']
 os.system(f"echo '📄 PDF file located here: {paper_path}'")
 
+!python -m spacy download en_core_web_lg
 
-
-
+model = spacy.load('en_core_web_lg')
 
 
