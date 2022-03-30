8f496f5f3f4e21ec3988407d2b1081023b9947b0
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-30 13:00:51 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,13 +6,23 @@ import spacy
 import os
 os.system(f"echo '🎉 All imports OK'")
 
-paper_path = os.environ['PAPER_PATH']
-os.system(f"echo '📄 PDF file located here: {paper_path}'")
+fold_loc = 'https://github.com/JoshWilde/test-python-action/tree/main/Author_Folders'
+print(fold_loc)
 
-os.system('python -m spacy download en_core_web_lg')
+folds = glob.glob(fold_loc)
+print(folds)
+
+folds = glob.glob(fold_loc+'/*')
+print(folds)
+
+
+#paper_path = os.environ['PAPER_PATH']
+#os.system(f"echo '📄 PDF file located here: {paper_path}'")
+
+#os.system('python -m spacy download en_core_web_lg')
 
 #model = en_core_web_lg.load()
-model = spacy.load('en_core_web_lg')
+#model = spacy.load('en_core_web_lg')
 print('SUCCESS!')
 
 #!python -m spacy download en_core_web_lg
