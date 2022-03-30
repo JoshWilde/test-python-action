1780e7edb1bc999ea2aa8e926edf02f0c4bdd85d
JoshWilde <45201519+JoshWilde@users.noreply.github.com>
2022-03-30 15:18:26 +0100

    Updated how glob files loaded

--- Author_Papers.py
+++ Author_Papers.py
@@ -6,6 +6,7 @@ import pdfplumber
 import PyPDF2
 import nltk
 import spacy
+import re
 
 from nltk import word_tokenize
 from nltk import download
@@ -31,6 +32,10 @@ os.system(f"echo '🎉 All imports OK'")
 
 folder_names = os.environ['FOLDER_PATH']
 os.system(f"echo '📄 PDF file located here: {folder_names}'")
+
+pdfs = os.environ['GLOB_PDFS']
+print(pdfs)
+pdfs = pdfs.split(' ')
 
 os.system('python -m spacy download en_core_web_lg')
 model = spacy.load('en_core_web_lg')
@@ -185,19 +190,34 @@ def Author_vectors_TF(folder_names, model, num_top20=20, directory_offset=21):
             
     ''' 
     Author_Dict = {} # Defines an empty dictionary
-    for k in range(len(folder_names)): # For each author
+    for i in range(len(list(folder_names))): # For each author
+        paper_list = folder_names[list(folder_names)[i]]
         #print(folder_names[k][directory_offset:]+ ' - ' +str(k))
-        paper_list = glob.glob(folder_names[k]+'/*.pdf') # Finds all PDF files in this folder
+        #paper_list = glob.glob(folder_names[k]+'/*.pdf') # Finds all PDF files in this folder
         print(paper_list)
         average_vector = Reviewer_Paper_Vector(paper_list, model, num_top20) # Generates the average vector for all the papers in this folder
-        Author_Dict[folder_names[k][directory_offset:]] = average_vector # Adds this average vector to the dictionary
+        Author_Dict[folder_names[i]] = average_vector # Adds this average vector to the dictionary
     return Author_Dict
-  
+    
+  def Make_Folder_dict(pdfs):
+    Master_dict = {}
+    for i in range(len(pdfs)):
+     #print(pdfs[i])
+     J = re.search('/', pdfs[i])
+     K = re.search('/',pdfs[i][J.end():])
+     Folder_name = pdfs[i][J.end():J.end()+K.start()]
+     pdf_name = pdfs[i]#[J.end()+K.end():]
+     if Folder_name not in Master_dict:
+      Master_dict[Folder_name] = [pdf_name]
+     else:
+      Master_dict[Folder_name].append(pdf_name)
+    return Master_dict
+
 print('folder names')
-print(folder_names)
-folds = glob.glob(folder_names+'/*')
-print(folds)
-Author_Dict = Author_vectors_TF(folds, model)
+
+
+Master_dict = Make_Folder_dict(pdfs)
+Author_Dict = Author_vectors_TF(Master_dict, model)
 
 print(Author_Dict)
 np.save('Author_Dict_generated.npy', Author_Dict)
